34c394f9492f26c9e12e51c11e116aeadea5fb67
qchenevier <qchenevier@users.noreply.github.com>
2017-06-02 16:22:48 +0200

    Functionalization for readability

--- wrangle_openstreetmap_data.py
+++ wrangle_openstreetmap_data.py
@@ -1,32 +1,36 @@
 import xml.etree.cElementTree as ET
 
-from builtins import * # python 2 compatibility
+from builtins import *  # python 2 compatibility
 from pprint import pprint
 from collections import Counter
 
 
 def get_cursor(file_handler):
-    return ET.iterparse(file_handler, events=('start','end'))
+    return ET.iterparse(file_handler, events=('start', 'end'))
 
 
-def get_tags(filename, levels=False):
-    with open(filename) as f:
-        cursor = get_cursor(f)
+def get_tags(filename, with_levels=False):
+    def _update_level(event, level):
+        level += 1 if event == 'start' else -1
+        return level
+
+    def _append_tag(tags, element, event, level, with_levels=False):
+        if event == 'start':
+            tags.append((element.tag, level) if with_levels else element.tag)
+        return tags
+
+    with open(filename) as file_h:
+        cursor = get_cursor(file_h)
         level = -1
         tags = []
         for event, element in cursor:
-            if event == 'start':
-                level += 1
-                if levels:
-                    tags.append((element.tag, level))
-                else:
-                    tags.append(element.tag)
-            elif event == 'end':
-                level -= 1
+            level = _update_level(event, level)
+            tags = _append_tag(
+                tags, element, event, level, with_levels=with_levels)
     return tags
 
 
 FILENAME = 'toulouse_small.osm'
 
 Counter(get_tags(FILENAME))
-Counter(get_tags(FILENAME, levels=True))
+Counter(get_tags(FILENAME, with_levels=True))
