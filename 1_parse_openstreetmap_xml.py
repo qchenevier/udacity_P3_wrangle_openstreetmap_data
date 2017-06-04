f3e1037c26da07b87eb8761cb66ff1737ba0544b
qchenevier <qchenevier@users.noreply.github.com>
2017-06-04 17:04:06 +0200

    Added reporting of xml parsing progress (useful for very large files)

--- 1_parse_openstreetmap_xml.py
+++ 1_parse_openstreetmap_xml.py
@@ -1,10 +1,15 @@
 import re
 import xml.etree.cElementTree as ET
 import pymongo
+import logging
 
 from builtins import *  # python 2 compatibility
 from collections import Counter
 from datetime import datetime
+
+
+def _get_cursor(file_handler, events=('start', 'end')):
+    return ET.iterparse(file_handler, events=events)
 
 
 def _convert_type(data):
@@ -69,17 +74,43 @@ def _add_record(data, element, recursive=True):
 
 
 def parse_data(filename, filter_list=['node', 'way', 'relation'], recursive=True):
+    def _report_parsing_progress(cursor_line, num_lines):
+        logging.info(
+            'Parsed {} / {} lines. ({:.1f} %)'
+            .format(cursor_line, num_lines, cursor_line / num_lines * 100)
+        )
+
     with open(filename) as file_h:
+        cursor = _get_cursor(file_h, events=('start',))
+        logging.info('Counting lines to parse.')
+        cursor_line = 0
+        for event, element in cursor:
+            cursor_line += 1
+            if cursor_line % 500000 == 0:
+                logging.info('{} lines read.'.format(cursor_line))
+            element.clear()
+        num_lines = cursor_line
+        logging.info('{} lines to parse.'.format(num_lines))
+
+    with open(filename) as file_h:
+        cursor_line = 0
         cursor = _get_cursor(file_h, events=('start',))
         data = {}
         for event, element in cursor:
+            cursor_line += 1
             if element.tag in filter_list:
                 data = _add_record(data, element, recursive=recursive)
+            if cursor_line % 100000 == 0:
+                _report_parsing_progress(cursor_line, num_lines)
+            element.clear()
+    _report_parsing_progress(cursor_line, num_lines)
     return data
 
 
+logging.basicConfig(level=logging.DEBUG)
 # FILENAME = 'toulouse_extra_small.osm'
-FILENAME = 'toulouse_medium.osm'
+# FILENAME = 'toulouse_medium.osm'
+FILENAME = 'toulouse_large.osm'
 
 collection_names = ['node', 'way', 'relation']
 osm = parse_data(FILENAME, filter_list=collection_names)
@@ -89,4 +120,5 @@ client = pymongo.MongoClient()
 client.drop_database('osm')
 db = client['osm']
 for category in collection_names:
+    logging.info("inserting collection '{}' in mongoDB".format(category))
     db[category].insert(osm[category])
